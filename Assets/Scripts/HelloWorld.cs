504a63b346899a62a60d7f8d5431d65fcd2034d5
antoniofdez76 <jose.fernandez@udgvirtual.udg.mx>
2023-01-28 21:44:13 -0600

    Initial commit

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+
+    //String 
+    string saludo = "Hola mundo, Feliz año 2023";
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(saludo);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
